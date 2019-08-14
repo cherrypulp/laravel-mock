857add21f5e4ea589db36ddc7ec4c9744d2b37a3
Daniel Sum <daniel@lacerisenumerique.be>
2019-08-14 23:53:02 +0200

    - handle factory and validation case

--- config/mock.php
+++ config/mock.php
@@ -2,10 +2,25 @@
 
 return [
     "route" => "api-mock", // Default route path of the mock api
-    "path" => "mock", // Default folder path of the mock api
+    "path" => "storage/mock", // Default folder path of the mock api
     "paginate" => true, // Define if we must paginate the results or not
-    "per_page" => 1, // Define the number of items per page (if paginate = true)
+    "per_page" => 10, // Define the number of items per page (if paginate = true)
     "condition" => function(){ // The condition to define if the mock routing is enabled or not
         return app()->isLocal();
-    }
+    },
+    "force_json" => true, // Force the whole mock controller to render application/json
+    "entrypoints" => [ // instead of looking into a folder you can also run a factory($class$, $number$)->states($state$)->make($override$) or add validation on controller method
+        /*"users" => [
+            "class" => \App\User::class, // if null factory won't be triggerred
+            "number" => 100, // number of factory to run
+            "states" => [],
+            "override" => [],
+            "update_validation" => [
+                "name" => "required"
+            ],
+            "update_force_json" => false,
+            "index_force_json" => false,
+            "store_validation" => \App\Http\Requests\UserPostRequest::class
+        ]*/
+    ]
 ];
--- src/Http/Controllers/MockController.php
+++ src/Http/Controllers/MockController.php
@@ -2,6 +2,7 @@
 
 namespace Blok\Mock\Http\Controllers;
 
+use App\Http\Requests\UserPostRequest;
 use Blok\Mock\Mock;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -9,6 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use phpDocumentor\Reflection\File;
+use Symfony\Component\HttpFoundation\HeaderBag;
 
 /**
  * Data mock controller into json (for testing an mocking)
@@ -17,31 +21,89 @@ class MockController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    /**
+     * @param $table
+     * @param $method
+     * @param $request Request
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function _handleValidation($table, $method, $request){
+
+        $validation = config("mock.entrypoints.".$table.".".$method."_validation");
+
+        if (config("mock.force_json") || config("mock.entrypoints.".$table.".".$method."_force_json")) {
+            $request->server->set('HTTP_ACCEPT', 'application/json');
+            $request->headers = new HeaderBag($request->server->getHeaders());
+        }
+
+        if ($validation) {
+            if (is_array($validation)) {
+                $this->validate($request, $validation);
+            } else {
+                /**
+                 * @var $formRequest UserPostRequest
+                 */
+                $formRequest = new $validation($request->query->all(), $request->request->all(), $request->attributes->all(), $request->cookies->all(), $request->files->all(), $request->server->all(), $request->getContent());
+
+                $this->validate($formRequest, $formRequest->rules());
+            }
+        }
+    }
+
+    /**
+     * @param $table
+     * @param Request $request
+     * @return array|\Illuminate\Http\JsonResponse|LengthAwarePaginator|Collection|mixed
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function index($table, Request $request)
     {
-        $folder = public_path(Mock::getPath() . '/' . $table);
+        $this->_handleValidation($table, "index", $request);
 
-        if (is_file(Mock::getPath() . '/' . $table . '.json')) {
-            $data = json_decode(file_get_contents(Mock::getPath() . '/' . $table . '.json'), true);
-            return response()->json($data);
-        }
+        $entity = config("mock.entrypoints.".$table);
 
-        if (!is_dir($folder)) {
-            \File::makeDirectory($folder, 0775, true);
-        }
+        if ($entity) {
 
-        $files = \File::allFiles(public_path(Mock::getPath() . '/' . $table));
+            /**
+             * @var $data Collection
+             */
+            $data = factory($entity["class"], $entity["number"])->states($entity["states"])->make($entity["override"]);
 
-        $data = [];
+            $data = $data->map(function ($item, $key) {
 
-        foreach ($files as $file) {
-            $item = json_decode(file_get_contents($file->getRealPath()), true);
+                if (!$item["id"]) {
+                    $item['id'] = $key + 1;
+                }
 
-            if (!isset($item["id"])) {
-                $item["id"] = str_replace(".json", "", $file->getBasename());
+                return $item;
+            });
+
+        } else {
+
+            $folder = public_path(Mock::getPath() . '/' . $table);
+
+            if (is_file(Mock::getPath() . '/' . $table . '.json')) {
+                $data = json_decode(file_get_contents(Mock::getPath() . '/' . $table . '.json'), true);
+                return response()->json($data);
             }
 
-            $data[] = $item;
+            if (!is_dir($folder)) {
+                \File::makeDirectory($folder, 0775, true);
+            }
+
+            $files = \File::allFiles(public_path(Mock::getPath() . '/' . $table));
+
+            $data = [];
+
+            foreach ($files as $file) {
+                $item = json_decode(file_get_contents($file->getRealPath()), true);
+
+                if (!isset($item["id"])) {
+                    $item["id"] = str_replace(".json", "", $file->getBasename());
+                }
+
+                $data[] = $item;
+            }
         }
 
         if (config("mock.paginate")) {
@@ -69,8 +131,16 @@ class MockController extends BaseController
         return $data;
     }
 
+    /**
+     * @param $table
+     * @param Request $request
+     * @return array
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store($table, Request $request)
     {
+        $this->_handleValidation($table, "store", $request);
+
         $folder = public_path(Mock::getPath() . '/' . $table);
 
         if (!is_dir($folder)) {
@@ -80,14 +150,23 @@ class MockController extends BaseController
         $data = $request->all();
         $data['id'] = uniqid();
 
-        file_put_contents($folder . '/' . $data['id'] . '.json', json_encode($data));
+       \File::put($folder . '/' . $data['id'] . '.json', json_encode($data));
 
         return $data;
     }
 
+    /**
+     * @param $table
+     * @param $id
+     * @param Request $request
+     * @return array
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function update($table, $id, Request $request)
     {
-        $folder = public_path(Mock::getPath() . $table . '/' . $id);
+        $this->_handleValidation($table, "update", $request);
+
+        $folder = public_path(Mock::getPath() ."/". $table);
 
         if (!is_dir($folder)) {
             \File::makeDirectory($folder, 493, true);
@@ -95,16 +174,45 @@ class MockController extends BaseController
 
         $data = $request->all();
 
-        file_put_contents($folder . '/' . $data['id'] . '.json', json_encode($data));
+        if (!isset($data["id"])) {
+            $data["id"] = $id;
+        }
+
+        \File::put($folder . '/' . $data['id'] . '.json', json_encode($data));
 
         return $data;
     }
 
-    public function view($table, $id)
+    /**
+     * @param $table
+     * @param $id
+     * @param Request $request
+     * @return Collection|mixed
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function view($table, $id, Request $request)
     {
-        $folder = public_path(Mock::getPath() . '/' . $table);
+        $this->_handleValidation($table, "view", $request);
 
-        $data = json_decode(file_get_contents($folder . '/' . $id . '.json'), true);
+        $entity = config("mock.entrypoints.".$table);
+
+        if ($entity) {
+
+            /**
+             * @var $data Collection
+             */
+            $data = factory($entity["class"])->states($entity["states"])->make($entity["override"]);
+
+            if (!$data->id) {
+                $data->id = $id;
+            }
+
+        } else {
+
+            $folder = public_path(Mock::getPath() . '/' . $table);
+
+            $data = json_decode(file_get_contents($folder . '/' . $id . '.json'), true);
+        }
 
         return $data;
     }
